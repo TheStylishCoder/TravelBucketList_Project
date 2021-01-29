dc845398b16abe32ba47408be7b18d1f974125b1
Anna Cunningham <ac232@hotmail.co.uk>
2021-01-29 19:26:27 +0000

    Further country tests passing

--- tests/country_tests.py
+++ tests/country_tests.py
@@ -9,3 +9,12 @@ class TestCountry(unittest.TestCase):
 
     def test_country_has_name(self):
         self.assertEqual("Japan", self.country.name)
+
+    def test_country_has_cities(self):
+        self.assertEqual(3, len(self.country.cities))
+
+    def test_country_visited_starts_false(self):
+        self.assertEqual(False, self.country.visited)
+
+    def test_country_wishlist_starts_false(self):
+        self.assertEqual(False, self.country.wishlist)
