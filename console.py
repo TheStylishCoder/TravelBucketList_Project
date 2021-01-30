22f086b142182d1641a5ff3ccb768fcb776da968
Anna Cunningham <ac232@hotmail.co.uk>
2021-01-30 15:29:18 +0000

    Inserted attraction objects into table

--- console.py
+++ console.py
@@ -64,6 +64,56 @@ city_repository.save(city10)
 
 attraction1 = Attraction("Sensoji Temple", "Place of Worship", city1)
 attraction_repository.save(attraction1)
+attraction2 = Attraction("Imperial Palace", "Historic Sight", city1)
+attraction_repository.save(attraction2)
+attraction3 = Attraction("Ghibli Museum", "Museum", city1, True)
+attraction_repository.save(attraction3)
+
+attraction4 = Attraction("Nishiki Market", "Food Market", city2)
+attraction_repository.save(attraction4)
+attraction5 = Attraction("Nijo Castle", "Historic Sight", city2, True)
+attraction_repository.save(attraction5)
+
+attraction6 = Attraction("Museum of Modern Art", "Museum", city3)
+attraction_repository.save(attraction6)
+
+attraction7 = Attraction("Botanical Gardens", "Parks & Nature", city4, True)
+attraction_repository.save(attraction7)
+attraction8 = Attraction("St.Louis Cathedral", "Place of Worship", city4)
+attraction_repository.save(attraction8)
+
+attraction9 = Attraction("Fenway Park", "Sports", city5, True)
+attraction_repository.save(attraction9)
+
+attraction10 = Attraction("Bondi Beach", "Beach", city6)
+attraction_repository.save(attraction10)
+attraction11 = Attraction("Taronga Zoo", "Zoo", city6)
+attraction_repository.save(attraction11)
+
+attraction12 = Attraction("El Retiro Park", "Parks & Nature", city7)
+attraction_repository.save(attraction12)
+attraction13 = Attraction("Royal Palace", "Historical Sight", city7, True)
+attraction_repository.save(attraction13)
+attraction14 = Attraction("Gran Via", "Food & Shopping", city7)
+attraction_repository.save(attraction14)
+
+attraction15 = Attraction("Camp Nou", "Sports", city8)
+attraction_repository.save(attraction15)
+
+attraction16 = Attraction("Buckingham Palace", "Historical Sight", city9, True)
+attraction_repository.save(attraction16)
+attraction17 = Attraction("Spitalfields Market", "Market", city9)
+attraction_repository.save(attraction17)
+attraction18 = Attraction("British Museum", "Museum", city9)
+attraction_repository.save(attraction18)
+
+attraction19 = Attraction("Kelvingrove Museum", "Museum", city10)
+attraction_repository.save(attraction19)
+attraction20 = Attraction("Glasgow Cathedral", "Place of Worship", city10)
+attraction_repository.save(attraction20)
+
+
+
 
 pdb.set_trace()
 
