4d53e1801176c4fe668cf9c29ddd3f57df769c18
Anna Cunningham <ac232@hotmail.co.uk>
2021-01-31 10:54:23 +0000

    Hello World! appears

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from controllers.review_controller import reviews_blueprint
 from controllers.attraction_controller import attractions_blueprint
