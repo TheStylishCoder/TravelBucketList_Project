e312e3bd6971fca8674d7b9f470261fcf9e30323
Anna Cunningham <ac232@hotmail.co.uk>
2021-01-31 11:48:05 +0000

    Added option to edit countries

--- controllers/country_controller.py
+++ controllers/country_controller.py
@@ -16,3 +16,17 @@ def show(id):
     country = country_repository.select(id)
     cities = country_repository.cities(country)
     return render_template("countries/show.html", country = country, cities = cities)
+
+@countries_blueprint.route("/countries/<id>/edit", methods=['GET'])
+def edit_country(id):
+    country = country_repository.select(id)
+    return render_template("countries/edit.html", country = country)
+
+@countries_blueprint.route("/countries/<id>", methods=['POST'])
+def update_country(id):
+    name = request.form['name']
+    visited = request.form['visited']
+    wishlist = request.form['wishlist']
+    country = Country(name, visited, wishlist, id)
+    country_repository.update(country)
+    return redirect('/countries')
