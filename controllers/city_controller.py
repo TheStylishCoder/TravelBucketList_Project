66e87ea0fd28c536fa00ec220a74084c25c1ab8a
Anna Cunningham <ac232@hotmail.co.uk>
2021-01-31 14:26:00 +0000

    Can edit/update cities

--- controllers/city_controller.py
+++ controllers/city_controller.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 
 from models.city import City
 import repositories.city_repository as city_repository
+import repositories.country_repository as country_repository
 
 cities_blueprint = Blueprint("cities", __name__)
 
@@ -16,4 +17,20 @@ def show(id):
     city = city_repository.select(id)
     attractions = city_repository.attractions(city)
     return render_template("cities/show.html", city = city, attractions = attractions)
-    
+
+@cities_blueprint.route("/cities/<id>/edit", methods=['GET'])
+def edit_city(id):
+    city = city_repository.select(id)
+    countries = country_repository.select_all()
+    return render_template("cities/edit.html", city = city, countries = countries)
+
+@cities_blueprint.route("/cities/<id>", methods=['POST'])
+def update_city(id):
+    name = request.form['name']
+    country_id = request.form['country_id']
+    visited = request.form['visited']
+    wishlist = request.form['wishlist']
+    country = country_repository.select(country_id)
+    city = City(name, country, visited, wishlist, id)
+    city_repository.update(city)
+    return redirect("/cities")
